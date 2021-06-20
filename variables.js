71da83d5c2407c4459b7be9e373c18f3b6f81594
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-06-20 12:10:06 -0500

    commented all of the code

--- variables.js
+++ variables.js
@@ -1,100 +1,100 @@
-//Declare and initialize a variable
-
-let testVar = 9;
-
-console.log(testVar);
-console.log("The value of testVar is", testVar);
-testVar = 7654456
-console.log(testVar);
-console.log("The value of testVar is", testVar);
-
-
-
-let fruitName = "banana";
-
-let arrayNum = [];
-
-
-for(let i = 0; i < fruitName.length; i++){
-  if(fruitName[i] == "b"){
-    arrayNum.push("b")
-  }else if(fruitName[i] ==="a"){
-    arrayNum.push("a")
-  }else if(fruitName[i] === "n"){
-    arrayNum.push("n")
-  }else if(fruitName[i] === "s"){
-    arrayNum.push("a")
-  }else if(fruitName[i] === "n"){
-    arrayNum.push("n")
-  } else if(fruitName[i] ==="a"){
-    arrayNum.push("a")
-  }
-}
-
-
-console.log(arrayNum); // output ["b", "a", "n", "a", "n", "a"]
-
-// + operator can concatenate 2 strings to gether or
-
-//Example
-  let firstName = " Hamza";
-  let lastName = "Mohamed";
-//example
-  let fullName = firstName + " " + lastName;
-
-  console.log(fullName);
-// + add to number together
-//Example
-let num = 9;
-let num1 = 2;
-let total = num  + num1;
-
-console.log(total); // output is 11
-
-let plants = 12 ;
-//plants  = plants + 3;
-
-console.log(plants);// another way to write this
-
-plants+=4;
-console.log(plants);// another way to write this
-
-plants++;
-console.log(plants);// another way to write this
-
-plants-=4;
-console.log(plants);// another way to write this
-
-
-let motorCycles = 9;
-let cars = 0
-console.log("had to put the soft tail in the garage");
-
-motorCycles--; //incrementing motorCycles by 1;
-
-console.log("Bikes available: ", motorCycles)
-console.log("I bought 2 lambos");
-cars += 2;
-
-console.log("Cars: ", cars);
-
-console.log("Wanted a Jetta, so I got one");
-
-cars++;
-console.log("Cars: ", cars);
-
-let catName = "Squiggle";
-let catAge = 3;
-console.log(catName);
-
-//concatenation with a comma for console (comma only works in the console.log)
-console.log("The name of the cat is:", catName);
-console.log("The age of the is:", catAge);
-
-//concatenation text with "+"
-
-let sentence = "This is a sentence about the cate named " + catName;
-
-sentence = "The cat name is " + catName + " and the cat is " + catAge + " years old"
-
-console.log(sentence);
+// //Declare and initialize a variable
+//
+// let testVar = 9;
+//
+// console.log(testVar);
+// console.log("The value of testVar is", testVar);
+// testVar = 7654456
+// console.log(testVar);
+// console.log("The value of testVar is", testVar);
+//
+//
+//
+// let fruitName = "banana";
+//
+// let arrayNum = [];
+//
+//
+// for(let i = 0; i < fruitName.length; i++){
+//   if(fruitName[i] == "b"){
+//     arrayNum.push("b")
+//   }else if(fruitName[i] ==="a"){
+//     arrayNum.push("a")
+//   }else if(fruitName[i] === "n"){
+//     arrayNum.push("n")
+//   }else if(fruitName[i] === "s"){
+//     arrayNum.push("a")
+//   }else if(fruitName[i] === "n"){
+//     arrayNum.push("n")
+//   } else if(fruitName[i] ==="a"){
+//     arrayNum.push("a")
+//   }
+// }
+//
+//
+// console.log(arrayNum); // output ["b", "a", "n", "a", "n", "a"]
+//
+// // + operator can concatenate 2 strings to gether or
+//
+// //Example
+//   let firstName = " Hamza";
+//   let lastName = "Mohamed";
+// //example
+//   let fullName = firstName + " " + lastName;
+//
+//   console.log(fullName);
+// // + add to number together
+// //Example
+// let num = 9;
+// let num1 = 2;
+// let total = num  + num1;
+//
+// console.log(total); // output is 11
+//
+// let plants = 12 ;
+// //plants  = plants + 3;
+//
+// console.log(plants);// another way to write this
+//
+// plants+=4;
+// console.log(plants);// another way to write this
+//
+// plants++;
+// console.log(plants);// another way to write this
+//
+// plants-=4;
+// console.log(plants);// another way to write this
+//
+//
+// let motorCycles = 9;
+// let cars = 0
+// console.log("had to put the soft tail in the garage");
+//
+// motorCycles--; //incrementing motorCycles by 1;
+//
+// console.log("Bikes available: ", motorCycles)
+// console.log("I bought 2 lambos");
+// cars += 2;
+//
+// console.log("Cars: ", cars);
+//
+// console.log("Wanted a Jetta, so I got one");
+//
+// cars++;
+// console.log("Cars: ", cars);
+//
+// let catName = "Squiggle";
+// let catAge = 3;
+// console.log(catName);
+//
+// //concatenation with a comma for console (comma only works in the console.log)
+// console.log("The name of the cat is:", catName);
+// console.log("The age of the is:", catAge);
+//
+// //concatenation text with "+"
+//
+// let sentence = "This is a sentence about the cate named " + catName;
+//
+// sentence = "The cat name is " + catName + " and the cat is " + catAge + " years old"
+//
+// console.log(sentence);
