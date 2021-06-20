f8d506716ef84540bab3d555a5cf54c61f5d07ed
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-06-20 10:44:12 -0500

    How to push string into an empty array using for

--- variables.js
+++ variables.js
@@ -10,26 +10,26 @@ console.log("The value of testVar is", testVar);
 
 
 
-// let fruitName = "banana";
-//
-// let arrayNum = [];
-//
-//
-// for(let i = 0; i < fruitName.length; i++){
-//   if(fruitName[i] == "b"){
-//     arrayNum.push("b")
-//   }else if(fruitName[i] ==="a"){
-//     arrayNum.push("a")
-//   }else if(fruitName[i] === "n"){
-//     arrayNum.push("n")
-//   }else if(fruitName[i] === "s"){
-//     arrayNum.push("a")
-//   }else if(fruitName[i] === "n"){
-//     arrayNum.push("n")
-//   } else if(fruitName[i] ==="a"){
-//     arrayNum.push("a")
-//   }
-// }
-//
-//
-// console.log(arrayNum)
+let fruitName = "banana";
+
+let arrayNum = [];
+
+
+for(let i = 0; i < fruitName.length; i++){
+  if(fruitName[i] == "b"){
+    arrayNum.push("b")
+  }else if(fruitName[i] ==="a"){
+    arrayNum.push("a")
+  }else if(fruitName[i] === "n"){
+    arrayNum.push("n")
+  }else if(fruitName[i] === "s"){
+    arrayNum.push("a")
+  }else if(fruitName[i] === "n"){
+    arrayNum.push("n")
+  } else if(fruitName[i] ==="a"){
+    arrayNum.push("a")
+  }
+}
+
+
+console.log(arrayNum)
