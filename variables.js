22a057598c4446d4fa29365aad21820a38807fb3
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-06-20 10:41:36 -0500

    Varaibles practical practice

---
+++ variables.js
@@ -0,0 +1,35 @@
+//Declare and initialize a variable
+
+let testVar = 9;
+
+console.log(testVar);
+console.log("The value of testVar is", testVar);
+testVar = 7654456
+console.log(testVar);
+console.log("The value of testVar is", testVar);
+
+
+
+// let fruitName = "banana";
+//
+// let arrayNum = [];
+//
+//
+// for(let i = 0; i < fruitName.length; i++){
+//   if(fruitName[i] == "b"){
+//     arrayNum.push("b")
+//   }else if(fruitName[i] ==="a"){
+//     arrayNum.push("a")
+//   }else if(fruitName[i] === "n"){
+//     arrayNum.push("n")
+//   }else if(fruitName[i] === "s"){
+//     arrayNum.push("a")
+//   }else if(fruitName[i] === "n"){
+//     arrayNum.push("n")
+//   } else if(fruitName[i] ==="a"){
+//     arrayNum.push("a")
+//   }
+// }
+//
+//
+// console.log(arrayNum)
